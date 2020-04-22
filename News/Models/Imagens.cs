16791c1359a8e7595f5445c8298c3994f103a3ac
claudia <aluno20619@ipt.pt>
2020-04-22 18:48:01 +0100

    Classe e os seus relacionamentos

--- News/Models/Imagens.cs
+++ News/Models/Imagens.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,10 @@ namespace News.Models
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Legenda { get; set; }
-        public int NoticiasId { get; set; }
+
+
+        [ForeignKey(nameof(NoticiaId))]
+        public int NoticiasFK { get; set; }
+        public Noticias NoticiaId { get; set; }
     }
 }
--- News/Models/Partilha.cs
+++ News/Models/Partilha.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,18 @@ namespace News.Models
 {
     public class Partilha
     {
+        //Não se é assim que se junta as duas numa chave primária
+        [Key]
+        public Utilizadores UtilizadorId { get; set; }
+        [Key]
+        public Noticias NoticiaId { get; set; }
+
+        [ForeignKey(nameof(NoticiaId))]
+        public int NoticiasFK { get; set; }
+
+        [ForeignKey(nameof(UtilizadorId))]
+        public int UtilizadoresFK { get; set; }
         
-        public int UtilizadoresId { get; set; }
-        public int NoticiasId { get; set; }
+
     }
 }
