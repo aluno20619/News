eac56baefbccca4532392d37ce1d301211f07a5b
ToCBarros <48324552+ToCBarros@users.noreply.github.com>
2020-04-28 00:23:48 +0100

    Update Noticias.cs

--- News/Models/Noticias.cs
+++ News/Models/Noticias.cs
@@ -8,10 +8,15 @@ namespace News.Models
 {
     public class Noticias
     {
+        public Noticias() {
+         ListaNoticias = new HashSet<Noticias>();
+        }
         [Key]
         public int Id { get; set; }
         public string Titulo { get; set; }
         public string Texto1 { get; set; }
         public string Texto2 { get; set; }
+        public ICollection<Imagens> ListaImagens { get; set; }
+        public ICollection<Partilha> ListaPartilha { get; set; }
     }
 }
