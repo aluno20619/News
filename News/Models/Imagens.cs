a44a130ccf15228d27ef9b0deec8ea84df47ac24
ToCBarros <48324552+ToCBarros@users.noreply.github.com>
2020-04-28 00:26:26 +0100

    Update Imagens.cs

--- News/Models/Imagens.cs
+++ News/Models/Imagens.cs
@@ -9,6 +9,9 @@ namespace News.Models
 {
     public class Imagens
     {
+        public Imagens() {
+         ListaImagens = new HashSet<Imagens>();
+        }
         [Key]
         public int Id { get; set; }
         public string Nome { get; set; }
